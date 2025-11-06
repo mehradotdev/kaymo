1a5c5b9288aeccfcd092e87d205cf7d51ac50879
mehra <mehradotdev@gmail.com>
2025-11-06 13:04:46 +0530

    chore: integrate Convex with Nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,132 @@
-import Image from "next/image";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "~/convex/_generated/api";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <>
+      <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
+        Convex + Next.js
+      </header>
+      <main className="p-8 flex flex-col gap-16">
+        <h1 className="text-4xl font-bold text-center">Convex + Next.js</h1>
+        <Content />
       </main>
+    </>
+  );
+}
+
+function Content() {
+  const { viewer, numbers } =
+    useQuery(api.myFunctions.listNumbers, {
+      count: 10,
+    }) ?? {};
+  const addNumber = useMutation(api.myFunctions.addNumber);
+
+  if (viewer === undefined || numbers === undefined) {
+    return (
+      <div className="mx-auto">
+        <p>loading... (consider a loading skeleton)</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-8 max-w-lg mx-auto">
+      <p>Welcome {viewer ?? "Anonymous"}!</p>
+      <p>
+        Click the button below and open this page in another window - this data
+        is persisted in the Convex cloud database!
+      </p>
+      <p>
+        <button
+          className="bg-foreground text-background text-sm px-4 py-2 rounded-md"
+          onClick={() => {
+            void addNumber({ value: Math.floor(Math.random() * 10) });
+          }}
+        >
+          Add a random number
+        </button>
+      </p>
+      <p>
+        Numbers:{" "}
+        {numbers?.length === 0
+          ? "Click the button!"
+          : numbers?.join(", ") ?? "..."}
+      </p>
+      <p>
+        Edit{" "}
+        <code className="text-sm font-bold font-mono bg-slate-200 dark:bg-slate-800 px-1 py-0.5 rounded-md">
+          convex/myFunctions.ts
+        </code>{" "}
+        to change your backend
+      </p>
+      <p>
+        Edit{" "}
+        <code className="text-sm font-bold font-mono bg-slate-200 dark:bg-slate-800 px-1 py-0.5 rounded-md">
+          app/page.tsx
+        </code>{" "}
+        to change your frontend
+      </p>
+      <p>
+        See the{" "}
+        <Link href="/server-example" className="underline hover:no-underline">
+          /server-example route
+        </Link>{" "}
+        for an example of loading data in a server component
+      </p>
+      <div className="flex flex-col">
+        <p className="text-lg font-bold">Useful resources:</p>
+        <div className="flex gap-2">
+          <div className="flex flex-col gap-2 w-1/2">
+            <ResourceCard
+              title="Convex docs"
+              description="Read comprehensive documentation for all Convex features."
+              href="https://docs.convex.dev/home"
+            />
+            <ResourceCard
+              title="Stack articles"
+              description="Learn about best practices, use cases, and more from a growing
+            collection of articles, videos, and walkthroughs."
+              href="https://www.typescriptlang.org/docs/handbook/2/basic-types.html"
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-1/2">
+            <ResourceCard
+              title="Templates"
+              description="Browse our collection of templates to get started quickly."
+              href="https://www.convex.dev/templates"
+            />
+            <ResourceCard
+              title="Discord"
+              description="Join our developer community to ask questions, trade tips & tricks,
+            and show off your projects."
+              href="https://www.convex.dev/community"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ResourceCard({
+  title,
+  description,
+  href,
+}: {
+  title: string;
+  description: string;
+  href: string;
+}) {
+  return (
+    <div className="flex flex-col gap-2 bg-slate-200 dark:bg-slate-800 p-4 rounded-md h-28 overflow-auto">
+      <a href={href} className="text-sm underline hover:no-underline">
+        {title}
+      </a>
+      <p className="text-xs">{description}</p>
     </div>
   );
 }
